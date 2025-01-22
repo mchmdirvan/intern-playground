b4f8baf637eab93539e203fa5b11f203fd424454
Mochamad Irvan <mchmdirvaan@gmail.com>
2025-01-22 16:53:22 +0700

    materi 9

--- 02_PhP_WPU/Materi/Materi_9/01_Variable_Scope.php
+++ 02_PhP_WPU/Materi/Materi_9/01_Variable_Scope.php
@@ -2,17 +2,23 @@
 // Variable Scope
 // Variable Scope adalah ruang lingkup sebuah variabel yang menentukan di mana variabel tersebut dapat diakses atau dimanipulasi.
 // Variabel PHP memiliki 3 ruang lingkup:
-// 1. Local Scope
-// 2. Global Scope
+// 1. Global Scope
+// 2. Local Scope
 // 3. Static Scope
 
-// 1. Local Scope
+// 1. Global Scope
+// Variabel yang dideklarasikan di luar fungsi memiliki ruang lingkup global dan dapat diakses di mana saja dalam skrip PHP.
+$x = 10; // global scope
+echo $x;
+
+// 2. Local Scope
 // Variabel yang dideklarasikan di dalam fungsi hanya dapat diakses di dalam fungsi tersebut.
-function myFunction()
+function tampilX()
 {
-  $x = 5; // local scope
-  echo "<p>Variable x inside function is: $x</p>";
+  $x = 20; // local scope
+  echo "Variable x inside function is: $x";
 }
+
 
 
 ?>
